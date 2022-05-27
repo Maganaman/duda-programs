f96e7c9d5545ce1bb3e2b5f949914d88abaff4d8
Maganaman <106397692+Maganaman@users.noreply.github.com>
2022-05-27 10:50:41 -0600

    Create newWorld.js

---
+++ newWorld.js
@@ -0,0 +1,7 @@
+<script>
+alert("newWorld");
+  const myFirstElement = <h1>Hello React!</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myFirstElement);
+</script>
