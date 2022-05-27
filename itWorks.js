d6adc4b6c141f8a981f913d533764d4107765c09
Maganaman <106397692+Maganaman@users.noreply.github.com>
2022-05-27 11:25:13 -0600

    Create itWorks.js

---
+++ itWorks.js
@@ -0,0 +1,43 @@
+(function() {
+  function drawChart(container) {
+    Highcharts.chart(container, {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: options.title
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      },{
+        name: 'John',
+        data: [5, 7, 3]
+      }]
+    });
+  }
+
+  var handler = {
+    // these methods must be named init and clean
+
+    init: function(options){
+      dmAPI.loadScript('https://code.highcharts.com/highcharts.js').then(function(){
+        drawChart(options.container);
+      });
+    },
+
+    clean: function(options) {
+      options.container.innerHTML = '';
+    }
+  }
+
+  dmAPI.registerExternalWidget('chart', handler)
+})();
