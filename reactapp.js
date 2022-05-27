217f6116db61c6a1dda790e0a1ee17e7f263e158
Maganaman <106397692+Maganaman@users.noreply.github.com>
2022-05-27 11:16:31 -0600

    Update reactapp.js

--- reactapp.js
+++ reactapp.js
@@ -1,25 +1,43 @@
-function drawChart(container) {
-  Highcharts.chart(container, {
-    chart: {
-      type: 'bar'
-    },
-    title: {
-      text: 'Fruit Consumption'
-    },
-    xAxis: {
-      categories: ['Apples', 'Bananas', 'Oranges']
-    },
-    yAxis: {
+(function() {
+  function drawChart(container) {
+    Highcharts.chart(container, {
+      chart: {
+        type: 'bar'
+      },
       title: {
-        text: 'Fruit eaten'
-      }
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      },{
+        name: 'John',
+        data: [5, 7, 3]
+      }]
+    });
+  }
+
+  var handler = {
+    // these methods must be named init and clean
+
+    init: function(options){
+      dmAPI.loadScript('https://code.highcharts.com/highcharts.js').then(function(){
+        drawChart(options.container);
+      });
     },
-    series: [{
-      name: 'Jane',
-      data: [1, 0, 4]
-    },{
-      name: 'John',
-      data: [5, 7, 3]
-    }]
-  });
-}
+
+    clean: function(options) {
+      options.container.innerHTML = '';
+    }
+  }
+
+  dmAPI.registerExternalWidget('chart', handler)
+})();
